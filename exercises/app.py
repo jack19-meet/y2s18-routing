2b8ff174013f7f7a49fc3430c539df170964a4dd
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 14:09:18 +0300

    done with the exercises but not the lab

--- exercises/app.py
+++ exercises/app.py
@@ -2,8 +2,14 @@ from databases import *
 from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+add_student("hh",9,True)
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template("home.html")
+
+@app.route('/student/<int:student_id>')
+def display_student(student_id):
+    stud=query_by_id(student_id)
+    return render_template("student.html",student_id=student_id)
 
 app.run(debug=True)
